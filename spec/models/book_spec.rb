4ffcdc7a4c502cfb8052654920724bcf2a1db7fe
mike vallano <mikevallano@gmail.com>
2021-09-20 09:39:21 -0500

    Add term and match examples

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -8,20 +8,83 @@ describe Book, type: :model do
       .results.results.map{ |res| res.to_h['_source'] }
   end
 
-  it 'has a valid factory' do
-    book = build(:book)
-    expect(book).to be_valid
-  end
-
   describe 'elasticsearch', :elasticsearch do
     let(:critters) { 'cats' }
+    let(:author) { create(:author, first_name: 'Steven', last_name: 'Zimmers') }
     let(:title) { "#{critters} in space"}
-    let!(:book) { create(:book, :reindex, title: title) }
-    context 'with multi_match' do
+    let(:isbn) { rand(1000..2000).to_s }
+    let!(:book) { create(:book, :reindex, title: title, author: author, isbn: isbn) }
+
+    context 'with #term' do
+      # Syntax uses the field as the key, followed by an explicit 'value' key
+      it 'does NOT match on full, exact matched title' do
+        query_hash = {
+          term: {
+            title: {value: book.title}
+          }
+        }
+        expect(search_result(query_hash).first.try(:[], :id)).to be nil
+      end
+
+      it 'DOES match searching one word of the title' do
+        query_hash = {
+          term: {
+            title: {value: critters}
+          }
+        }
+        expect(search_result(query_hash).first.try(:[], :id)).to eq(book.id)
+      end
+
+      it 'does NOT match singular version of word in the title' do
+        query_hash = {
+          term: {
+            title: {value: critters.singularize}
+          }
+        }
+        expect(search_result(query_hash).first.try(:[], :id)).to be nil
+      end
+
+      it 'DOES match searching exact match on a keyword field' do
+        query_hash = {
+          term: {
+            isbn: {value: book.isbn}
+          }
+        }
+        expect(search_result(query_hash).first.try(:[], :id)).to eq(book.id)
+      end
+
+      it 'des NOT match searching partial match of keyword field' do
+        query_hash = {
+          term: {
+            isbn: {value: book.isbn.first(3)}
+          }
+        }
+        expect(search_result(query_hash).first.try(:[], :id)).to be nil
+      end
+    end
+
+    context 'with #match' do
+      # Note syntax with the singular field to be queried followed by the query string
+      it 'finds book when searching exact title' do
+        query_hash = {
+          match: { title: book.title }
+        }
+        expect(search_result(query_hash).first.try(:[], :id)).to eq(book.id)
+      end
+
+      it 'does NOT match singular version of word in the title' do
+        query_hash = {
+          match: { title: critters.singularize }
+        }
+        expect(search_result(query_hash).first.try(:[], :id)).to be nil
+      end
+    end
+
+    context 'with #multi_match' do
       it 'finds book when searching exact title' do
         query_hash = {
           multi_match: {
-            query: title,
+            query: book.title,
             fields: [:title]
           }
         }
@@ -43,6 +106,26 @@ describe Book, type: :model do
           multi_match: {
             query: critters.singularize,
             fields: [:title]
+          }
+        }
+        expect(search_result(query_hash).first.try(:[], :id)).to be nil
+      end
+
+      it 'matches when searching author name' do
+        query_hash = {
+          multi_match: {
+            query: author.last_name,
+            fields: [:title, :author]
+          }
+        }
+        expect(search_result(query_hash).first.try(:[], :id)).to eq(book.id)
+      end
+
+      it 'does NOT match part of the author name' do
+        query_hash = {
+          multi_match: {
+            query: author.last_name.singularize,
+            fields: [:title, :author]
           }
         }
         expect(search_result(query_hash).first.try(:[], :id)).to be nil
