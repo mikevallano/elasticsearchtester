90f201a00a794b13889c1a8fbd10dd01b7d52585
mike vallano <mikevallano@gmail.com>
2021-09-20 08:47:33 -0500

    Improve test structure

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,54 +1,51 @@
 require 'rails_helper'
 
 describe Book, type: :model do
+
+  def search_result(query_hash)
+    Book.__elasticsearch__
+      .search(query: query_hash)
+      .results.results.map{ |res| res.to_h['_source'] }
+  end
+
   it 'has a valid factory' do
     book = build(:book)
     expect(book).to be_valid
   end
 
   describe 'elasticsearch', :elasticsearch do
-    it 'searches elasticsearch successfully' do
-      title = 'cats in space'
-      book = create(:book, :reindex, title: title)
-      response = Book.__elasticsearch__.search(
-        query: {
+    let(:critters) { 'cats' }
+    let(:title) { "#{critters} in space"}
+    let!(:book) { create(:book, :reindex, title: title) }
+    context 'with multi_match' do
+      it 'finds book when searching exact title' do
+        query_hash = {
           multi_match: {
             query: title,
             fields: [:title]
           }
         }
-      ).results.results.map{ |res| res.to_h['_source'] }
-      expect(response.first[:id]).to eq(book.id)
-    end
-
-    context 'with multi_match' do
-      it 'finds book when searching exact title' do
-        title = 'cats in space'
-        book = create(:book, :reindex, title: title)
-        response = Book.__elasticsearch__.search(
-          query: {
-            multi_match: {
-              query: title,
-              fields: [:title]
-            }
-          }
-        ).results.results.map{ |res| res.to_h['_source'] }
-        expect(response.first[:id]).to eq(book.id)
+        expect(search_result(query_hash).first.try(:[], :id)).to eq(book.id)
       end
 
-      it 'finds book when searching partial title' do
-        title = 'cats in space'
-        book = create(:book, :reindex, title: title)
-        response = Book.__elasticsearch__.search(
-          query: {
-            multi_match: {
-              query: 'cat',
-              fields: [:title],
-              fuzziness: 1
-            }
+      it 'finds book when searching query contains a word in the title' do
+        query_hash = {
+          multi_match: {
+            query: critters,
+            fields: [:title]
           }
-        ).results.results.map{ |res| res.to_h['_source'] }
-        expect(response.first.try(:[], :id)).to eq(book.id)
+        }
+        expect(search_result(query_hash).first.try(:[], :id)).to eq(book.id)
+      end
+
+      it 'does NOT match on singular version of a word in a text field' do
+        query_hash = {
+          multi_match: {
+            query: critters.singularize,
+            fields: [:title]
+          }
+        }
+        expect(search_result(query_hash).first.try(:[], :id)).to be nil
       end
     end
   end
